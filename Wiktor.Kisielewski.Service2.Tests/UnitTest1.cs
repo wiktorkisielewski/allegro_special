21eb8c238f883a414a0f4a29287f91899c417b4d
wiktorkisielewski <wiktor.kisielewski@protonmail.com>
2022-05-24 18:21:24 +0200

    tests for all services (XUnit)

---
+++ Wiktor.Kisielewski.Service2.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using System.Net.Http;
+
+namespace Wiktor.Kisielewski.Service1.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+		//SOME TESTS	
+	}
+    }
+}
